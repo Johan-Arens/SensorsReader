23aceecf6ae778b89226e9d357ba8204013dc989
johan <johan.arens@gmail.com>
2018-07-12 19:59:48 -0400

    header added

--- python/SensorsRead.py
+++ python/SensorsRead.py
@@ -137,7 +137,7 @@ def SensorWorker(SensorName, SensorLocation, SensorAddress, SensorType):
              DryContact = "CLOSED"
          try:
            if int(SensorAddress) < 41:
-             GPIO.setup(SensorAddress), GPIO.IN, pull_up_down=GPIO.PUD_UP)
+             GPIO.setup(int(SensorAddress), GPIO.IN, pull_up_down=GPIO.PUD_UP)
              if GPIO.input(SensorAddress)):
                DryContact = "OPEN"
              else:
