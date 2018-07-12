ca67c8d1755a3d84fe87825b2941149d66296ad9
Johan-Arens <41028098+Johan-Arens@users.noreply.github.com>
2018-07-12 12:09:57 -0400

    Delete johan.py

--- python/johan.py
+++
@@ -1,6 +0,0 @@
-n = raw_input("Integer? ")#Pick an integer.  And remember, if raw_input is not supported by your OS, use input()
-n = int(n)#Defines n as the integer you chose. (Alternatively, you can define n yourself)
-if n < 10:
-    print ("number ",n," is smaller than 10")
-else:
-    print ("number ",n," is greater than 10")
